e8f1165f8d06a458d410aefe31b191a9650980a0
Jared Updike <jupdike@thebrain.com>
2016-12-19 19:38:53 -0500

    Global variables and some code to show that it works.

--- tsvg.ts
+++ tsvg.ts
@@ -14,7 +14,33 @@ var lib = fs.readFileSync('tsvg-lib.ts');
 //let tree = React.createElement('a', {}, []); // todo read in file!
 //console.log(tree.render());
 
+var regexDefine = new RegExp("(@[a-zA-Z0-9_\-]+):\w*([^;]+);", "g");
+
 var infilecontents = fs.readFileSync(infilename) + "";
+
+// returns a copy of the input string with template stuff pulled out
+// and key values pairs added to vals
+function getGlobals(input, vals) {
+	var match;
+	var copyInput = input;
+	while ((match = regexDefine.exec(input)) !== null) {
+		var full = match[0];
+		var lhs = match[1];
+		var rhs = match[2];
+    var k = lhs.replace('@','');
+		copyInput = copyInput.replace(full, '');
+		vals[k] = rhs; // possibly overwrite previous value
+	}
+  return copyInput;
+}
+
+var kvs = {};
+infilecontents = getGlobals(infilecontents, kvs);
+var valbits = [];
+for (let prop in kvs) {
+  valbits.push('"' + prop + '": ' + kvs[prop]);
+}
+var valStr = '{ ' + valbits.join(', ') + ' }';
 
 // fix for certain XML v. JSX 'problems' in infilecontents, like these:
 infilecontents = infilecontents.replace(/xmlns:/g, 'xmlns_');
@@ -35,7 +61,8 @@ ${pre}
 
   ${lib}
 
-  bind(TSVG.Helpers, function() {
+  var that = FakeElement.combineAttrs(TSVG.Helpers, ${valStr});
+  bind(that, function() {
     TSVG.Templates['${inkey}'] = ${infilecontents}
   })();
 
